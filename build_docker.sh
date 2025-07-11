5357e46725c61a1ec213cc43a3e5de7909656155
Felipe Menanteau <felipe@illinois.edu>
2025-07-11 14:02:16 -0500

    Update build script and Dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -1,15 +1,24 @@
 
 # Build container
-export IMAGE=desthumbs
+export IMAGE=des_cutter
 export SPTUSER=$USER
 #export TAG=`date +"%Y%B%d"`
 export DES_CUTTER_VERSION=0.1
 export TAG=${DES_CUTTER_VERSION}
+
+# Check for optional --no-cache argument
+NO_CACHE=""
+if [[ "$1" == "--no-cache" ]]; then
+    NO_CACHE="--no-cache"
+    echo "Building without cache..."
+fi
+
 docker build -f docker/Dockerfile \
        -t menanteau/$IMAGE:$TAG \
        --build-arg DES_CUTTER_VERSION \
        --build-arg SPTUSER \
-       --rm=true .
+       --rm=true \
+       $NO_CACHE .
 
 echo 'Push commands:'
 echo "   docker push menanteau/$IMAGE:${TAG}"
